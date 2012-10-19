784a1e98d9f09fc2851f9931b0fc33fac7c5bc4d
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-10-19 11:54:13 +0000

    Added tabview widget to PrimeUI
    git-svn-id: http://primefaces.googlecode.com/svn/prime-ui/trunk@8177 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

---
+++ src/main/webapp/resources/js/tabview/tabview.js
@@ -0,0 +1,337 @@
+/**
+ * PrimeFaces TabView Widget
+ */
+PrimeFaces.widget.TabView = PrimeFaces.widget.BaseWidget.extend({
+    
+    init: function(cfg) {
+        this._super(cfg);
+        
+        if(!this.jq.hasClass('ui-tabs')) {
+            this.enhanceMarkup();
+        }
+        
+        this.navContainer = this.jq.children('.ui-tabs-nav');
+        this.panelContainer = this.jq.children('.ui-tabs-panels');
+        this.stateHolder = $(this.jqId + '_activeIndex');
+        this.cfg.selected = parseInt(this.stateHolder.val());
+        this.onshowHandlers = [];
+
+        this.bindEvents();
+
+        //Cache initial active tab
+        if(this.cfg.dynamic && this.cfg.cache) {
+            this.markAsLoaded(this.panelContainer.children().eq(this.cfg.selected));
+        }
+
+        this.jq.data('widget', this);
+    },
+    
+    bindEvents: function() {
+        var _self = this;
+
+        //Tab header events
+        this.navContainer.children('li')
+                .bind('mouseover.tabview', function(e) {
+                    var element = $(this);
+                    if(!element.hasClass('ui-state-disabled')) {
+                        element.addClass('ui-state-hover');
+                    }
+                })
+                .bind('mouseout.tabview', function(e) {
+                    var element = $(this);
+                    if(!element.hasClass('ui-state-disabled')) {
+                        element.removeClass('ui-state-hover');
+                    }
+                })
+                .bind('click.tabview', function(e) {
+                    var element = $(this);
+
+                    if($(e.target).is(':not(.ui-icon-close)')) {
+                        var index = element.index();
+
+                        if(!element.hasClass('ui-state-disabled') && index != _self.cfg.selected) {
+                            _self.select(index);
+                        }
+                    }
+
+                    e.preventDefault();
+                });
+
+        //Closable tabs
+        this.navContainer.find('li .ui-icon-close')
+            .bind('click.tabview', function(e) {
+                var index = $(this).parent().index();
+                
+                if(_self.cfg.onTabClose) {
+                    var retVal = _self.cfg.onTabClose.call(_self, index);
+                    
+                    if(retVal !== false) {
+                        _self.remove(index);
+                    }
+                }
+                else {
+                    _self.remove(index);
+                }
+
+                e.preventDefault();
+            });
+    },
+    
+    /**
+     * Selects an inactive tab given index
+     */
+    select: function(index) {
+        //Call user onTabChange callback
+        if(this.cfg.onTabChange) {
+            var result = this.cfg.onTabChange.call(this, index);
+            if(result == false)
+                return false;
+        }
+
+        var newPanel = this.panelContainer.children().eq(index),
+        shouldLoad = this.cfg.dynamic && !this.isLoaded(newPanel);
+
+        //update state
+        this.stateHolder.val(index);
+        this.cfg.selected = index;
+
+        if(shouldLoad) {
+            this.loadDynamicTab(newPanel);
+        }
+        else {
+            if(this.hasBehavior('tabChange')) {
+                this.fireTabChangeEvent(newPanel);
+            }
+            else {
+                this.show(newPanel);
+            }
+        }
+
+        return true;
+    },
+    
+    show: function(newPanel) {
+        var headers = this.navContainer.children(),
+        oldHeader = headers.filter('.ui-state-active'),
+        newHeader = headers.eq(newPanel.index()),
+        oldPanel = this.panelContainer.children('.ui-tabs-panel:visible'),
+        _self = this;
+
+        //aria
+        oldPanel.attr('aria-hidden', true);
+        oldHeader.attr('aria-expanded', false);
+        newPanel.attr('aria-hidden', false);
+        newHeader.attr('aria-expanded', true);
+
+        if(this.cfg.effect) {
+                oldPanel.hide(this.cfg.effect.name, null, this.cfg.effect.duration, function() {
+                oldHeader.removeClass('ui-state-focus ui-tabs-selected ui-state-active');
+
+                newHeader.addClass('ui-state-focus ui-tabs-selected ui-state-active');
+                newPanel.show(_self.cfg.effect.name, null, _self.cfg.effect.duration, function() {
+                    _self.postTabShow(newPanel);
+                });
+            });
+        }
+        else {
+            oldHeader.removeClass('ui-state-focus ui-tabs-selected ui-state-active');
+            oldPanel.hide();
+
+            newHeader.addClass('ui-state-focus ui-tabs-selected ui-state-active');
+            newPanel.show();
+
+            this.postTabShow(newPanel);
+        }
+    },
+    
+    /**
+     * Loads tab contents with ajax
+     */
+    loadDynamicTab: function(newPanel) {
+        var _self = this,
+        options = {
+            source: this.id,
+            process: this.id,
+            update: this.id
+        },
+        tabindex = newPanel.index();
+
+        options.onsuccess = function(responseXML) {
+            var xmlDoc = $(responseXML.documentElement),
+            updates = xmlDoc.find("update");
+
+            for(var i=0; i < updates.length; i++) {
+                var update = updates.eq(i),
+                id = update.attr('id'),
+                content = update.text();
+
+                if(id == _self.id){
+                    newPanel.html(content);
+
+                    if(_self.cfg.cache) {
+                        _self.markAsLoaded(newPanel);
+                    }
+                }
+                else {
+                    PrimeFaces.ajax.AjaxUtils.updateElement.call(this, id, content);
+                }
+            }
+
+            PrimeFaces.ajax.AjaxUtils.handleResponse.call(this, xmlDoc);
+
+            return true;
+        };
+
+        options.oncomplete = function() {
+            _self.show(newPanel);
+        };
+        
+        options.params = [
+            {name: this.id + '_contentLoad', value: true},
+            {name: this.id + '_newTab', value: newPanel.attr('id')},
+            {name: this.id + '_tabindex', value: tabindex}
+        ];
+
+        if(this.hasBehavior('tabChange')) {
+            var tabChangeBehavior = this.cfg.behaviors['tabChange'];
+
+            tabChangeBehavior.call(this, newPanel, options);
+        }
+        else {
+            PrimeFaces.ajax.AjaxRequest(options);
+        }
+    },
+    
+    /**
+     * Removes a tab with given index
+     */
+    remove: function(index) {    
+        var header = this.navContainer.children().eq(index),
+        panel = this.panelContainer.children().eq(index);
+
+        this.fireTabCloseEvent(panel);
+
+        header.remove();
+        panel.remove();
+
+        //active next tab if active tab is removed
+        if(index == this.cfg.selected) {
+            var newIndex = this.cfg.selected == this.getLength() ? this.cfg.selected - 1: this.cfg.selected;
+            this.select(newIndex);
+        }
+    },
+    
+    getLength: function() {
+        return this.navContainer.children().length;
+    },
+    
+    getActiveIndex: function() {
+        return this.cfg.selected;
+    },
+    
+    fireTabChangeEvent: function(panel) {
+        var tabChangeBehavior = this.cfg.behaviors['tabChange'],
+        _self = this,
+        ext = {
+            params: [
+                {name: this.id + '_newTab', value: panel.attr('id')},
+                {name: this.id + '_tabindex', value: panel.index()}
+            ]
+        };
+        
+        ext.oncomplete = function() {
+            _self.show(panel);
+        };
+
+        tabChangeBehavior.call(this, panel, ext);
+    },
+    
+    fireTabCloseEvent: function(panel) {    
+        if(this.hasBehavior('tabClose')) {
+            var tabCloseBehavior = this.cfg.behaviors['tabClose'],
+            ext = {
+                params: [
+                    {name: this.id + '_closeTab', value: panel.attr('id')},
+                    {name: this.id + '_tabindex', value: panel.index()}
+                ]
+            };
+
+            tabCloseBehavior.call(this, null, ext);
+        }
+    },
+    
+    hasBehavior: function(event) {
+        if(this.cfg.behaviors) {
+            return this.cfg.behaviors[event] != undefined;
+        }
+
+        return false;
+    },
+    
+    markAsLoaded: function(panel) {
+        panel.data('loaded', true);
+    },
+    
+    isLoaded: function(panel) {
+        return panel.data('loaded') == true;
+    },
+    
+    disable: function(index) {
+        this.navContainer.children().eq(index).addClass('ui-state-disabled');
+    },
+    
+    enable: function(index) {
+        this.navContainer.children().eq(index).removeClass('ui-state-disabled');
+    },
+    
+    addOnshowHandler: function(fn) {
+        this.onshowHandlers.push(fn);
+    },
+    
+    postTabShow: function(newPanel) {    
+        //execute user defined callback
+        if(this.cfg.onTabShow) {
+            this.cfg.onTabShow.call(this, newPanel);
+        }
+
+        //execute onshowHandlers and remove successful ones
+        this.onshowHandlers = $.grep(this.onshowHandlers, function(fn) {
+            return !fn.call();
+        });
+    },
+    
+    enhanceMarkup: function() {
+        this.jq.addClass('ui-tabs ui-widget ui-widget-content ui-corner-all ui-hidden-container')
+                .children('ul').addClass('ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all')
+                .children('li').addClass('ui-state-default ui-corner-top');
+                
+        this.jq.addClass('ui-tabs-' + this.cfg.orientation);
+               
+        this.jq.children('div').addClass('ui-tabs-panels').children().addClass('ui-tabs-panel ui-widget-content ui-corner-bottom');
+        
+        this.jq.find('> ul.ui-tabs-nav > li').eq(this.cfg.activeIndex).addClass('ui-tabs-selected ui-state-active');
+        this.jq.find('> div.ui-tabs-panels > div.ui-tabs-panel:not(:eq(' + this.cfg.activeIndex + '))').addClass('ui-helper-hidden');
+    }
+
+});
+
+/**
+ * jQuery Plugin for TabView Widget
+ */
+(function( $ ) {
+  
+  $.fn.tabview = function(options) {
+      
+        var cfg = $.extend({
+            activeIndex:0
+            ,orientation:'top'
+        }, options);
+            
+      return this.each(function() {
+         cfg.id = this.id;
+         
+         new PrimeFaces.widget.TabView(cfg);
+      });
+  };
+  
+})(jQuery);
