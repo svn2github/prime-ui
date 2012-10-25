d6037de7cc90a7fb75e58dbfee37a601768a4d8b
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2012-10-25 09:43:10 +0000

    rating widget migration to PrimeUI
    git-svn-id: http://primefaces.googlecode.com/svn/prime-ui/trunk@8190 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/webapp/resources/js/rating/rating.js
+++ src/main/webapp/resources/js/rating/rating.js
@@ -1,163 +1,85 @@
 /**
- * PrimeFaces Rating Widget
+ * PrimeUI Spinner widget
  */
-PrimeFaces.widget.Rating = PrimeFaces.widget.BaseWidget.extend({
-    
-    init: function(cfg) {
-        this._super(cfg);
-        
-        if(!this.jq.hasClass('ui-rating')) {
-            this.enhanceMarkup();
-        }
-        
-        this.jqInput = $(this.jqId + '_input');
-        this.value = this.getValue();
-        this.stars = this.jq.children('.ui-rating-star');
-        this.cancel = this.jq.children('.ui-rating-cancel');
-        
-        if(!this.cfg.disabled && !this.cfg.readonly) {
-            this.bindEvents();
-        }
-        
-        if(this.cfg.readonly) {
-            this.jq.children().css('cursor', 'default');
-        }
-    },
-    
-    bindEvents: function() {
-        var _self = this;
-        
-        this.stars.click(function() {
-            var value = _self.stars.index(this) + 1;   //index starts from zero
-            
-            _self.setValue(value);
-        });
-        
-        this.cancel.hover(function() {
-            $(this).toggleClass('ui-rating-cancel-hover');
-        })
-        .click(function() {
-            _self.reset();
-        });
-    },
-    
-    unbindEvents: function() {        
-        this.stars.unbind('click');
-        
-        this.cancel.unbind('hover click');
-    },
-    
-    getValue: function() {
-        var inputVal = this.jqInput.val();
-        
-        return inputVal == '' ? null : parseInt(inputVal);
-    },
-    
-    setValue: function(value) {
-        //set hidden value
-        this.jqInput.val(value);
-        
-        //update visuals
-        this.stars.removeClass('ui-rating-star-on');
-        for(var i = 0; i < value; i++) {
-            this.stars.eq(i).addClass('ui-rating-star-on');
-        }
-        
-        //invoke callback
-        if(this.cfg.onRate) {
-            this.cfg.onRate.call(this, value);
-        }
+$(function() {
 
-        //invoke ajax rate behavior
-        if(this.cfg.behaviors) {
-            var rateBehavior = this.cfg.behaviors['rate'];
-            if(rateBehavior) {
-                rateBehavior.call(this);
-            }
-        }
-    },
-    
-    enable: function() {
-        this.cfg.disabled = false;
-        
-        this.bindEvents();
-        
-        this.jq.removeClass('ui-state-disabled');
-    },
-    
-    disable: function() {
-        this.cfg.disabled = true;
-        
-        this.unbindEvents();
-        
-        this.jq.addClass('ui-state-disabled');
-    },
-    
-    reset: function() {
-        this.jqInput.val('');
-        
-        this.stars.filter('.ui-rating-star-on').removeClass('ui-rating-star-on');
-        
-        //invoke ajax cancel behavior
-        if(this.cfg.behaviors) {
-            var cancelBehavior = this.cfg.behaviors['cancel'];
-            if(cancelBehavior) {
-                cancelBehavior.call(this);
-            }
-        }
-    },
-    
-    enhanceMarkup: function() {
-        var input = this.jq.children('input'),
-        inputVal = input.val(),
-        value = inputVal == '' ? null : parseInt(inputVal);
-        
-        this.jq.addClass('ui-rating');
-        
-        input.attr({
-            id: this.id + '_input'
-            ,name: this.id + '_input'
-        })
-        
-        if(this.cfg.cancel) {
-            this.jq.append('<div class="ui-rating-cancel"><a></a></div>');
-        }
-        
-        for(var i = 0; i < this.cfg.stars; i++) {
-            var styleClass = (value > i) ? "ui-rating-star ui-rating-star-on" : "ui-rating-star";
-
-            this.jq.append('<div class="' + styleClass + '"><a></a></div>');
-        }
-        
-        if(input.prop('disabled')) {
-            this.cfg.disabled = true;
-            this.jq.addClass('ui-state-disabled');
-        }
-    }
-});
-
-/**
- * jQuery Plugin for Rating Widget
- */
-(function( $ ) {
-  
-  $.fn.rating = function(options) {
-      
-        var cfg = $.extend({
+    $.widget("primeui.rating", {
+       
+        options: {
             stars: 5,
             cancel: true
-        }, options);
+        },
+        
+        _create: function() {
+            var input = this.element;
             
-      return this.each(function() {
-          var input = $(this),
-          id = input.attr('id');
-          
-          input.wrap('<div id="' + id + '"/>').removeAttr('id');
-         
-          cfg.id = id;
-         
-          new PrimeFaces.widget.Rating(cfg);
-      });
-  };
-  
-})(jQuery);
+            input.wrap('<div />');
+            this.container = input.parent();
+            this.container.addClass('ui-rating');
+            
+            var inputVal = input.val(),
+            value = inputVal == '' ? null : parseInt(inputVal);
+            
+            if(this.options.cancel) {
+                this.container.append('<div class="ui-rating-cancel"><a></a></div>');
+            }
+
+            for(var i = 0; i < this.options.stars; i++) {
+                var styleClass = (value > i) ? "ui-rating-star ui-rating-star-on" : "ui-rating-star";
+
+                this.container.append('<div class="' + styleClass + '"><a></a></div>');
+            }
+            
+            this.stars = this.container.children('.ui-rating-star');
+
+            if(input.prop('disabled')) {
+                this.container.addClass('ui-state-disabled');
+            }
+            else if(!input.prop('readonly')){
+                this._bindEvents();
+            }
+        },
+        
+        _bindEvents: function() {
+            var $this = this;
+
+            this.stars.click(function() {
+                var value = $this.stars.index(this) + 1;   //index starts from zero
+
+                $this._setValue(value);
+            });
+
+            this.container.children('.ui-rating-cancel').hover(function() {
+                $(this).toggleClass('ui-rating-cancel-hover');
+            })
+            .click(function() {
+                $this.cancel();
+            });
+        },
+        
+        cancel: function() {
+            this.element.val('');
+        
+            this.stars.filter('.ui-rating-star-on').removeClass('ui-rating-star-on');
+        },
+        
+        _getValue: function() {
+            var inputVal = this.element.val();
+
+            return inputVal == '' ? null : parseInt(inputVal);
+        },
+
+        _setValue: function(value) {
+            this.element.val(value);
+
+            //update visuals
+            this.stars.removeClass('ui-rating-star-on');
+            for(var i = 0; i < value; i++) {
+                this.stars.eq(i).addClass('ui-rating-star-on');
+            }
+            
+            this._trigger('rate');
+        }
+    });
+    
+});
