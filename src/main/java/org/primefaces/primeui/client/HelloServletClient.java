713df14a06c4b44937dfbfb65cc33a78ee64f5f5
cagatay.civici@gmail.com <cagatay.civici@gmail.com@5a70e874-a5cb-11dd-8af2-493fb0bb9f7c>
2013-05-04 11:31:15 +0000

    Sample update
    git-svn-id: http://primefaces.googlecode.com/svn/prime-ui/trunk@9124 5a70e874-a5cb-11dd-8af2-493fb0bb9f7c

--- src/main/java/org/primefaces/primeui/client/HelloServletClient.java
+++ src/main/java/org/primefaces/primeui/client/HelloServletClient.java
@@ -2,31 +2,35 @@ package org.primefaces.primeui.client;
 
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.GenericType;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.config.ClientConfig;
 import com.sun.jersey.api.client.config.DefaultClientConfig;
-import java.net.URI;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriBuilder;
+import com.sun.jersey.api.json.JSONConfiguration;
+import java.util.Collection;
+import java.util.List;
+import org.primefaces.primeui.service.Suggestion;
 
 public class HelloServletClient {
  
     public static void main(String[] args) {
-        ClientConfig config = new DefaultClientConfig();
-        Client client = Client.create(config);
-        WebResource service = client.resource(getBaseURI());
+        ClientConfig clientConfig = new DefaultClientConfig();
+        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
+        Client client = Client.create(clientConfig);
+		WebResource webResource = client.resource("http://localhost:8080/prime-ui/rest/autocomplete/rrr");
         
-
-        System.out.println(service.path("rest").path("hello").accept(MediaType.TEXT_PLAIN).get(ClientResponse.class).toString());
-
-        System.out.println(service.path("rest").path("hello").accept(MediaType.TEXT_PLAIN).get(String.class));
-
-        System.out.println(service.path("rest").path("hello").accept(MediaType.TEXT_XML).get(String.class));
-
-        System.out.println(service.path("rest").path("hello").accept(MediaType.TEXT_HTML).get(String.class));
-    }
-
-    private static URI getBaseURI() {
-        return UriBuilder.fromUri("http://localhost:8080/prime-ui").build();
+        //GET
+        ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
+        Collection<Suggestion> output = response.getEntity(new GenericType<List<Suggestion>>(){});
+        
+        for(Object o : output) {
+            System.out.println(o);
+        }
+        
+        //POST
+        webResource = client.resource("http://localhost:8080/prime-ui/rest/autocomplete/save");
+        response = webResource.type("application/json").post(ClientResponse.class, "{\"label\":\"testlabel\", \"value\":\"testvalue\"}");
+        String outputString = response.getEntity(String.class);
+        System.out.println("Save Response:" + outputString);
     }
 }
--- src/main/java/org/primefaces/primeui/service/AutoCompleteService.java
+++ src/main/java/org/primefaces/primeui/service/AutoCompleteService.java
@@ -15,30 +15,51 @@
  */
 package org.primefaces.primeui.service;
 
+import java.awt.print.Book;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import javax.xml.bind.JAXBElement;
 
 @Path("/autocomplete")
-@Produces(MediaType.APPLICATION_JSON)
 public class AutoCompleteService {
+
+    @Context
+    private UriInfo uriInfo;
     
     @GET
     @Path("{query}")
+    @Produces(MediaType.APPLICATION_JSON)
     public Collection<Suggestion> getSuggestions(@PathParam("query") String query) {
         List<Suggestion> suggestions = new ArrayList<Suggestion>();
-        for(int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {
             String label = query + i;
             String value = label + "_value";
-            
+
             suggestions.add(new Suggestion(label, value));
         }
-        
+
         return suggestions;
+    }
+
+    @POST
+    @Path("/save")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createNewBook(Suggestion suggestion) {
+        System.out.println(suggestion.getLabel() + "," + suggestion.getValue());
+
+        return Response.status(201).entity("suggestion saved").build();
     }
 }
